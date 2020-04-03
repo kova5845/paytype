08fae1721aa0f8d30d431c207f1b6a118736ed7d
alexey <lesha.kovalenko.1999@mail.ru>
2020-04-03 19:38:39 +0300

    ready

--- paytype/paytype/apps/catalog/views.py
+++ paytype/paytype/apps/catalog/views.py
@@ -28,7 +28,6 @@ def paytype_view_id(request, paytype_id):
     return render(request, 'paytype_view_id.html', {'paytype': paytype})
 
 
-
 def paytype_add(request):
     p_type = PaymentsType.objects.all()
     p_code = PaymentsCode.objects.all()
@@ -45,23 +44,45 @@ def paytype_add_add(request):
     paytype.description = request.POST['description']
     paytype.start_date = datetime.strptime(request.POST['start_date'], '%Y-%m-%d').date()
     paytype.finish_date = datetime.strptime(request.POST['finish_date'], '%Y-%m-%d').date()
-    paytype.feature = request.POST['feature']
+    if request.POST['feature'] == '':
+        feature = None
+    else:
+        feature = int(request.POST['country'])
+    paytype.feature = feature
     paytype.document = request.POST['document']
     paytype.route = request.POST['route']
-    paytype.save()
-    p_type = PaymentsType.objects.get(request.POST['p_type'])
+    p_type = PaymentsType.objects.get(payment_type=int(request.POST['p_type']))
     paytype.p_type = p_type
-    p_code = PaymentsType.objects.get(request.POST['p_code'])
+    p_code = PaymentsCode.objects.get(payment_code=int(request.POST['p_code']))
     paytype.p_code = p_code
-    countries = []
-    for cn in request.POST.getlist('countries'):
-        countries.append(Country.objects.get(name=cn))
-    paytype.countries.clear()
-    paytype.countries.set(countries)
-    not_countries = []
-    for cn in request.POST.getlist('not_countries'):
-        countries.append(Country.objects.get(name=cn))
-    paytype.not_countries.clear()
-    paytype.not_countries.set(not_countries)
     paytype.save()
-    return HttpResponseRedirect(reverse('paytype_view'))
+    if request.POST['country'] == '':
+        country = None
+    else:
+        country = Country.objects.get(code=int(request.POST['country']))
+    paytype.country = country
+    if request.POST['not_country'] == '':
+        not_country = None
+    else:
+        not_country = Country.objects.get(code=int(request.POST['not_country']))
+    paytype.not_country = not_country
+    if paytype.country == paytype.not_country:
+        paytype.not_country = None
+    # countries = []
+    for cn in request.POST.getlist('countries'):
+        paytype.countries.add(Country.objects.get(code=cn))
+    for cn in request.POST.getlist('not_countries'):
+        paytype.not_countries.add(Country.objects.get(code=cn))
+    # not_countries = []
+    # for cn in request.POST.getlist('not_countries'):
+    #     not_countries.append(Country.objects.get(code=cn))
+    # for c in countries:
+    #     if c in not_countries:
+    #         not_countries.remove(c)
+    # print(not_countries)
+    # print(countries)
+    # paytype.not_countries.set(request.POST.getlist('not_countries'))
+    # paytype.countries.set(request.POST.getlist('countries'))
+
+    paytype.save()
+    return HttpResponseRedirect(reverse('catalog:paytype_view'))
