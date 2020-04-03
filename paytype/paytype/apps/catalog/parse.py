f74ac335d4a4757b2406eac52b86f123a8fa8a37
alexey <lesha.kovalenko.1999@mail.ru>
2020-04-03 19:51:50 +0300

    parser

--- paytype/paytype/apps/catalog/parse.py
+++ paytype/paytype/apps/catalog/parse.py
@@ -14,7 +14,24 @@ with io.open('COUNTY.csv', encoding='utf-8') as file:
             p = Country(code=int(line['CODE']))
             p.save()
 
-# with io.open('COUNTY.csv', encoding='utf-8') as file:
-#     reader = csv.DictReader(file, delimiter=',')
-#     for line in reader:
-#         print(line['CODE'])
+with io.open('PAYMENSTYPE.csv', encoding='utf-8') as file:
+    reader = csv.DictReader(file, delimiter=',')
+    for line in reader:
+        try:
+            go = PaymentsType.objects.get(payment_type=int(line['PAYMENTTYPE']))
+        except PaymentsType.DoesNotExist:
+            go = None
+        if go == None:
+            p = PaymentsType(payment_type=int(line['PAYMENTTYPE']))
+            p.save()
+
+with io.open('PAYMENSTYPE.csv', encoding='utf-8') as file:
+    reader = csv.DictReader(file, delimiter=',')
+    for line in reader:
+        try:
+            go = PaymentsCode.objects.get(payment_code=int(line['PAYMENTCODE']))
+        except PaymentsCode.DoesNotExist:
+            go = None
+        if go == None:
+            p = PaymentsCode(payment_code=int(line['PAYMENTCODE']))
+            p.save()
